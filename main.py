32e79931605d86786d56dc048badbf4e81862b66
Ivan Kovalik <qwezx004@yandex.ru>
2022-10-10 13:35:46 +0700

    Another commit

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+class Person:
+
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+    def check(self):
+        if self.name == '' or self.email == '' or self.age == 0:
+            raise ValueError('You must provide a name, age and email')
+
+
 def main():
-    for i in range(10):
-        print("Running command " + i)
+    p = Person('name', 0, '')
+    print(p.check())
 
 
 main()
